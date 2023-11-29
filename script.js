d700ace24febf970933365842163b430ad3db69a
void-anurag <anuragtamhankar.0@gmail.com>
2023-11-30 02:14:32 +0530

    Added Play-a-round function

--- script.js
+++ script.js
@@ -12,6 +12,29 @@ function getComputerChoice(){
             null; 
     }
 }
+const computerSelection = getComputerChoice();
+
+function play_a_round(playerSelection, computerSelection){
+    const playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice === computerSelection.toLowerCase()){
+        return "It's a tie. Play again!";
+    }
+
+    if(
+        (playerChoice === 'rock' && computerSelection === 'scissors') ||
+        (playerChoice === 'paper' && computerSelection === 'rock') ||
+        (playerChoice === 'scissors' && computerSelection === 'paper')
+        )
+        {
+            return 'You Win! ${playerChoice} beats ${computerSelection}.';
+        }
+        
+        return 'You Lose. ${computerSelection} beats ${playerChoice}.';
+}
+
+const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
+console.log(play_a_round(playerSelection, computerSelection));
 
 //const computerChoice = getComputerChoice();
 //console.log(computerChoice);
