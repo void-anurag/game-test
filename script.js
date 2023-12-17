1176833ae2803a10ef9cc7fb4dfad256970f1731
void-anurag <anuragtamhankar.0@gmail.com>
2023-12-17 23:43:52 +0530

    All done

--- script.js
+++ script.js
@@ -1,78 +1,183 @@
-const choices = ["rock", "paper", "scissors"];
-let winners = [];
+const userScoreCard = document.querySelector(".score .user");
+const compScoreCard = document.querySelector(".score .comp");
+const rockButton = document.querySelector(".options .rock");
+const paperButton = document.querySelector(".options .paper");
+const scissorsButton = document.querySelector(".options .scissors");
+const status = document.querySelector(".status");
+const endStatus = document.querySelector(".game-end");
+const resetButton = document.querySelector("#reset");
 
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    playRound(i);
-  }
-  document.querySelector("button").textContent = "Play new game";
-  logWins();
+let userScore = 0;
+let compScore = 0;
+let statusString;
+let userChoice;
+let rounds = 0;
+
+let compChoice = randChoice();
+
+main();
+
+function randChoice() {
+	switch (Math.floor(Math.random() * 3) + 1) {
+		case 1:
+			return "rock";
+		case 2:
+			return "paper";
+		case 3:
+			return "scissors";
+	}
 }
 
-function playRound(round) {
-  const playerSelection = playerChoice();
-  const computerSelection = computerChoice();
-  const winner = checkWinner(playerSelection, computerSelection);
-  winners.push(winner);
-  logRound(playerSelection, computerSelection, winner, round);
+function main() {
+	rockButton.addEventListener("click", () => {
+		userChoice = "rock";
+		rounds++;
+		checkDecision(userChoice);
+		compChoice = randChoice();
+		checkRoundOver();
+	});
+
+	paperButton.addEventListener("click", () => {
+		userChoice = "paper";
+		rounds++;
+		checkDecision(userChoice);
+		compChoice = randChoice();
+		checkRoundOver();
+	});
+
+	scissorsButton.addEventListener("click", () => {
+		userChoice = "scissors";
+		rounds++;
+		checkDecision(userChoice);
+		compChoice = randChoice();
+		checkRoundOver();
+	});
+
+	resetButton.addEventListener("click", reset);
 }
 
-function playerChoice() {
-  let input = prompt("Type Rock, Paper, or Scissors");
-  while (input == null) {
-    input = prompt("Type Rock, Paper, or Scissors");
-  }
-  input = input.toLowerCase();
-  let check = validateInput(input);
-  while (check == false) {
-    input = prompt(
-      "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
-    );
-    while (input == null) {
-      input = prompt("Type Rock, Paper, or Scissors");
-    }
-    input = input.toLowerCase();
-    check = validateInput(input);
-  }
-  return input;
+function checkDecision(userChoice) {
+	switch (userChoice + compChoice) {
+		case "rockssissors":
+		case "paperrock":
+		case "scissorspaper":
+			win(userChoice);
+			break;
+		case "rockrock":
+		case "paperpaper":
+		case "scissorsscissors":
+			draw(userChoice);
+			break;
+		case "rockpaper":
+		case "scissorsrock":
+		case "paperscissors":
+			loss(compChoice);
+			break;
+	}
+	userScoreCard.innerHTML = userScore;
+	compScoreCard.innerHTML = compScore;
 }
 
-function computerChoice() {
-  return choices[Math.floor(Math.random() * choices.length)];
+function win(user) {
+	userScore++;
+	switch (user) {
+		case "rock":
+			statusString = "Rock crushed Scissors. ";
+			break;
+		case "paper":
+			statusString = "Paper covered Rocks. ";
+			break;
+		case "scissors":
+			statusString = "Scissors sliced Paper. ";
+			break;
+	}
+	status.innerHTML =
+		`Round ${rounds}: ` + statusString + "<strong>You</strong> win!.";
+
+	document.querySelector(`.${userChoice}`).classList.add("win");
+	setTimeout(
+		() => document.querySelector(`.${userChoice}`).classList.remove("win"),
+		600
+	);
 }
 
-function validateInput(choice) {
-  return choices.includes(choice);
+function loss(comp) {
+	compScore++;
+	switch (comp) {
+		case "rock":
+			statusString = "Rock crushed Scissors. ";
+			break;
+		case "paper":
+			statusString = "Paper covered Rocks. ";
+			break;
+		case "scissors":
+			statusString = "Scissors sliced Paper. ";
+			break;
+	}
+	status.innerHTML =
+		`Round ${rounds}: ` +
+		statusString +
+		"<strong>Comp</strong> wins!.";
+	document.querySelector(`.${userChoice}`).classList.add("loss");
+	setTimeout(
+		() => document.querySelector(`.${userChoice}`).classList.remove("loss"),
+		600
+	);
 }
 
-function checkWinner(choiceP, choiceC) {
-  if (choiceP === choiceC) {
-    return "Tie";
-  } else if (
-    (choiceP === "rock" && choiceC == "scissors") ||
-    (choiceP === "paper" && choiceC == "rock") ||
-    (choiceP === "scissors" && choiceC == "paper")
-  ) {
-    return "Player";
-  } else {
-    return "Computer";
-  }
+function draw(user) {
+	switch (user) {
+		case "rock":
+			statusString = "Two Rocks collided. ";
+			break;
+		case "paper":
+			statusString = "Two Papers stacked. ";
+			break;
+		case "scissors":
+			statusString = "Two Scissors crossed.";
+			break;
+	}
+	status.innerHTML =
+		`Round ${rounds}: ` + statusString + "<strong>Draw</strong>.";
+	document.querySelector(`.${userChoice}`).classList.add("draw");
+	setTimeout(
+		() => document.querySelector(`.${userChoice}`).classList.remove("draw"),
+		600
+	);
 }
 
-function logWins() {
-  let playerWins = winners.filter((item) => item == "Player").length;
-  let computerWins = winners.filter((item) => item == "Computer").length;
-  let ties = winners.filter((item) => item == "Tie").length;
-  console.log("Results:");
-  console.log("Player Wins:", playerWins);
-  console.log("Computer Wins:", computerWins);
-  console.log("Ties:", ties);
+function reset() {
+	userScore = 0;
+	compScore = 0;
+	if (
+		status.innerText ===
+			"Make a choice. The computer made its choice already." ||
+		status.innerHTML === "New Game! Good luck this time." ||
+		status.innerHTML === "Game is already reset."
+	) {
+		status.innerHTML = "Game is already reset.";
+	} else status.innerHTML = "New Game! Good luck this time.";
+	userScoreCard.innerHTML = userScore;
+	compScoreCard.innerHTML = compScore;
+	rockButton.disabled = false;
+	paperButton.disabled = false;
+	scissorsButton.disabled = false;
+	rounds = 0;
+	endStatus.innerHTML = "";
 }
 
-function logRound(playerChoice, computerChoice, winner, round) {
-  console.log("Round:", round);
-  console.log("Player Chose:", playerChoice);
-  console.log("Computer Chose:", computerChoice);
-  console.log(winner, "Won the Round");
-  console.log("-------------------------------");
+function checkRoundOver() {
+	if (rounds === 5) {
+		rockButton.disabled = true;
+		paperButton.disabled = true;
+		scissorsButton.disabled = true;
+
+		if (userScore > compScore) {
+			endStatus.innerHTML = `You win. You are awesome. <p>Play Again?</p>`;
+		} else if (compScore > userScore) {
+			endStatus.innerHTML = `You lose. Better luck next time. <p>Play Again?</p>`;
+		} else {
+			endStatus.innerHTML = `Looks like a draw to me. <p>Play Again?</p>`;
+		}
+	}
 }
